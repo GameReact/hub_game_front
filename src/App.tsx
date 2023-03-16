8fdb1595017a818b4d5895f94ab571e78889bdd8
ABOATDev <30906528+ABOATDev@users.noreply.github.com>
2023-03-16 09:54:15 +0100

    Add page about

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/home/Home';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/home/Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/global.css";
+import About from "./pages/about/about";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
   },
 ]);
 
