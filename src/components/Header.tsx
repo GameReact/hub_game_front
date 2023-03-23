793ee317840917dff907863816626165207acef0
ABOATDev <30906528+ABOATDev@users.noreply.github.com>
2023-03-23 11:49:24 +0100

    Fix link header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -95,13 +95,15 @@ function Header() {
 
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                <Link to={"/" + page}>{page}</Link>
-              </Button>
+              <Link to={"/" + page}>
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              </Link>
             ))}
           </Box>
 
