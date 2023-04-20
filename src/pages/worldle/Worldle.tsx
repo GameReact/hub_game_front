cdb5c3d961ce9d1541b45e26d62159f01c4f04e6
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2023-04-20 10:10:04 +0200

    Add Header and Footer in Worldle page

--- src/pages/worldle/Worldle.tsx
+++ src/pages/worldle/Worldle.tsx
@@ -1,11 +1,15 @@
+import { Footer } from "../../components/Footer";
+import { HeaderAction } from "../../components/Header";
 import WorldleGame from "../../components/worldle/WorldleGame";
 
 const Worldle: React.FunctionComponent = () => {
-    return (
-        <>
-            <WorldleGame/>
-        </>
-    );
+  return (
+    <>
+      <HeaderAction />
+      <WorldleGame />
+      <Footer />
+    </>
+  );
 };
 
 export default Worldle;
